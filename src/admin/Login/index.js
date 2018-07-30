41a1bf62fba23e18dcadf636e2311ad6ec304e1d
chwech <804699297@qq.com>
2018-07-30 23:07:27 +0800

    完成登录表单

--- src/admin/Login/index.js
+++ src/admin/Login/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-
+import './index.less'
 class LoginPage extends Component {
   render () {
     return (
-      <div>登录页面</div>
+      <div className="login">
+        <input className="username" placeholder="用户名" />
+        <input className="password" placeholder="密码"/>
+        <button className="submit-btn">登录</button>
+      </div>
     )
   }
 }
